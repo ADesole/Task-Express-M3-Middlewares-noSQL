e7b818962ca9741cf96cd44e87d13a2cc0d31dc5
Ali Akbar <Ali.A.Akbar98@hotmail.com>
2022-06-03 17:59:53 +0300

    Completed the task

--- api/posts/posts.controllers.js
+++ api/posts/posts.controllers.js
@@ -1,30 +1,43 @@
-const Post = require('../../models/Post');
+const Post = require("../../models/Post");
 
-exports.postsCreate = async (req, res) => {
+exports.postsCreate = async (req, res, next) => {
   try {
     const newPost = await Post.create(req.body);
     res.status(201).json(newPost);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
+    // res.status(500).json({ message: error.message });
   }
 };
 
-exports.postsDelete = async (req, res) => {
+exports.fetchPost = async (postId, next) => {
+  try {
+    console.log("it's doing it");
+    const foundPost = await Post.findById(postId);
+    res.json(foundPost);
+  } catch (error) {
+    next(error);
+    // res.status(500).json({ message: error.message });
+  }
+};
+
+exports.postsDelete = async (req, res, next) => {
   const { postId } = req.params;
   try {
-    const foundPost = await Post.findById(+postId);
+    const foundPost = await Post.findById(postId);
     if (foundPost) {
       await foundPost.remove();
       res.status(204).end();
     } else {
-      res.status(404).json({ message: 'post not found' });
+      res.status(404).json({ message: "post not found" });
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    // res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
-exports.postsUpdate = async (req, res) => {
+exports.postsUpdate = async (req, res, next) => {
   const { postId } = req.params;
   try {
     const foundPost = Post.findById(+postId);
@@ -32,18 +45,20 @@ exports.postsUpdate = async (req, res) => {
       await foundPost.findByIdAndUpdate(postId, req.body);
       res.status(204).end();
     } else {
-      res.status(404).json({ message: 'post not found' });
+      res.status(404).json({ message: "post not found" });
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    // res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
-exports.postsGet = async (req, res) => {
+exports.postsGet = async (req, res, next) => {
   try {
     const posts = await Post.find();
     res.json(posts);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    // res.status(500).json({ message: error.message });
+    next(error);
   }
 };
--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const postsRoutes = require('./api/posts/posts.routes');
-const connectDb = require('./database');
+const postsRoutes = require("./api/posts/posts.routes");
+const connectDb = require("./database");
+const slugify = require("slugify");
+const router = require("./api/posts/posts.routes");
 
 connectDb();
 app.use(express.json());
-app.use('/posts', postsRoutes);
+app.use("/posts", postsRoutes);
+
+router.use((req, res, next) => {
+  if (req.method === "POST") req.body.slug = slugify(req.body.title);
+  next();
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+  console.log("Error handled");
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: "not found" });
+  next();
+});
 
 app.listen(8000, () => {
-  console.log('The application is running on localhost:8000');
+  console.log("The application is running on localhost:8000");
 });
